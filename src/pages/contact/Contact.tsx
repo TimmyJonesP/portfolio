9d8e4371a2c21a8def5fff9eaaaa82fb62ed9a6f
Sandro <sandrocarioli43@gmail.com>
2024-06-28 12:15:21 -0300

    fijo

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,23 +1,10 @@
-import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Contact = () => {
   const [t] = useTranslation("global");
-  const [scrollPosition, setScrollPosition] = useState(0);
   const email = "sandrocarioli43@gmail.com";
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollPosition(window.scrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   const handleCopy = () => {
     navigator.clipboard
@@ -40,35 +27,10 @@ const Contact = () => {
   const handleResume = () => {
     window.open(cv, "_blank");
   };
-
-  const backgrounds = [
-    "bg-green-800",
-    "bg-blue-800",
-    "bg-red-800",
-    "bg-yellow-800",
-    "bg-orange-800",
-    "bg-pink-800",
-    "bg-cyan-800",
-    "bg-amber-700",
-    "bg-zinc-800",
-    "bg-violet-800",
-    "bg-rose-700",
-  ];
-
-  const calculateBackgroundIndex = () => {
-    const index = Math.floor(scrollPosition / (window.innerHeight - 100));
-    return Math.max(0, Math.min(index, backgrounds.length - 1));
-  };
-
-  const currentBackgroundIndex = calculateBackgroundIndex();
-
   return (
-    <main className="no-scrollbar bg-green-400 relative">
+    <main className="no-scrollbar bg-green-400">
       <div className="h-screen w-full self-center top-0 left-0 text-black leading-[0.8] z-10 sticky p-4 text-[12vw] md:text-[6vw] font-medium">
         <div className="px-4 h-full flex flex-col w-full self-center justify-center mt-[6vw]">
-          <span className="pb-16 text-[10vw] md:text-[5vw] text-right">
-            SCROLL
-          </span>
           <div className="flex flex-col items-start uppercase">
             <span
               onClick={handleResume}
@@ -126,14 +88,6 @@ const Contact = () => {
           </div>
         </div>
       </div>
-      {backgrounds.map((bgClass, index) => (
-        <article
-          key={index}
-          className={`absolute inset-0 ${
-            index === currentBackgroundIndex ? "" : "hidden"
-          } ${bgClass}`}
-        ></article>
-      ))}
     </main>
   );
 };
